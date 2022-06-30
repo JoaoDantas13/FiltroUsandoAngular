da4ba9bcf5d8e33bcda50e670df5f043e9f436a2
Joao Vitor Dantas Pereira da Silva <joao.silva@dotzmkt.com.br>
2022-06-30 16:53:16 -0300

    estudando filtro com angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,56 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'FiltroemAngular';
+
+  nome = 'João';
+
+  mudaCampo(event: any): void {
+    const valor = event.target.value;
+    const estadosFiltrado = [];
+    for (let i = 0; i < this.estadosIniciais.length; i++) {
+      if (
+        this.estadosIniciais[i].nome.toLowerCase().includes(valor.toLowerCase())
+      ) {
+        estadosFiltrado.push(this.estadosIniciais[i]);
+      }
+    }
+    console.log('valor', valor);
+    console.log('estadosFiltrado', estadosFiltrado);
+    this.estados = estadosFiltrado;
+  }
+
+  estadosIniciais = [
+    { nome: 'Acre', sigla: 'AC' },
+    { nome: 'Alagoas', sigla: 'AL' },
+    { nome: 'Amapá', sigla: 'AP' },
+    { nome: 'Amazonas', sigla: 'AM' },
+    { nome: 'Bahia', sigla: 'BA' },
+    { nome: 'Ceará', sigla: 'CE' },
+    { nome: 'Distrito Federal', sigla: 'DF' },
+    { nome: 'Espírito Santo', sigla: 'ES' },
+    { nome: 'Goiás', sigla: 'GO' },
+    { nome: 'Maranhão', sigla: 'MA' },
+    { nome: 'Mato Grosso', sigla: 'MT' },
+    { nome: 'Mato Grosso do Sul', sigla: 'MS' },
+    { nome: 'Minas Gerais', sigla: 'MG' },
+    { nome: 'Pará', sigla: 'PA' },
+    { nome: 'Paraíba', sigla: 'PB' },
+    { nome: 'Paraná', sigla: 'PR' },
+    { nome: 'Pernambuco', sigla: 'PE' },
+    { nome: 'Piauí', sigla: 'PI' },
+    { nome: 'Rio de Janeiro', sigla: 'RJ' },
+    { nome: 'Rio Grande do Norte', sigla: 'RN' },
+    { nome: 'Rio Grande do Sul', sigla: 'RS' },
+    { nome: 'Rondônia', sigla: 'RO' },
+    { nome: 'Roraima', sigla: 'RR' },
+    { nome: 'Santa Catarina', sigla: 'SC' },
+    { nome: 'São Paulo', sigla: 'SP' },
+    { nome: 'Sergipe', sigla: 'SE' },
+    { nome: 'Tocantins', sigla: 'TO' },
+  ];
+  estados = this.estadosIniciais;
 }
